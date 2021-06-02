24ad799e5129f1e002810b848b7a43be1b473292
MacioBBS <marciobbs@gmail.com>
2021-06-01 21:25:15 -0700

    Add content to the Recipe Component

--- cook-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cook-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+   recipes: Recipe[] = [
+      new Recipe('Recipe Bananada', 'Recipe description 1', 'https://madrerestaurants.com/wp-content/uploads/2020/05/DSC05512-scaled.jpg'),
+      new Recipe('Recipe Chile rellenos', 'Recipe description 2', 'https://madrerestaurants.com/wp-content/uploads/2019/09/chile-rellenos.png'),
+      new Recipe('Recipe Camarones', 'Recipe description 3', 'https://madrerestaurants.com/wp-content/uploads/2019/09/camarones-al-mojo.png')
+   ];
   constructor() { }
 
   ngOnInit(): void {
